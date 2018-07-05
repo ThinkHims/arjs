169af25a1ecc97d2c968ae7303d11d651acd7e9c
Himanshu <thinktank.himanshu@gmail.com>
2018-07-05 21:21:46 +0530

    initial commit

---
+++ public/custom.js
@@ -0,0 +1,20 @@
+// var box=document.getElementById("box");
+// console.log(box);
+
+// AFRAME.registerComponent('cursor-listener', {
+//   init: function () {
+//     var lastIndex = -1;
+//     var COLORS = ['red', 'green', 'blue'];
+//     this.el.addEventListener('click', function (evt) {
+//       lastIndex = (lastIndex + 1) % COLORS.length;
+//       this.setAttribute('material', 'color', COLORS[lastIndex]);
+//       console.log('I was clicked at: ', evt.detail.intersection.point);
+//     });
+//   }
+// });
+
+// window.onload=function(){
+// 	document.getElementById("box").addEventListener("click",function(){
+// 		alert(99);
+// 	});
+// }
